3afd4040e5c405d4031394a0e88d97d312312d3e
nam.d <nam.d@gmail.com>
2021-06-16 20:49:56 +0700

    init realdworld UI

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,503 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="header">
+      <div class="logo">conduit</div>
+      <div class="nav">
+        <ul>
+          <li>Home</li>
+          <li>Sign In</li>
+          <li>Sign Up</li>
+        </ul>
+      </div>
+    </div>
+    <div class="body">
+      <div class="banner">
+        <h1>conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+      <div class="wrapper">
+        <div class="articles">
+          <p class="global-feed">Global feed</p>
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <!-- begin item -->
+          <div class="item">
+            <div class="wrapper-info1">
+              <div class="info">
+                <div class="avatar">
+                  <img
+                    src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/c0.50.200.200a/p200x200/125468303_246786090573472_7323045617622215240_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=WthjLtoHPj4AX9p_ueb&_nc_ht=scontent-sin6-1.xx&tp=27&oh=f861312a3e1fbb5247de55eebee54cf8&oe=60CE3C8D"
+                    alt=""
+                  />
+                </div>
+                <div class="wrapper-info">
+                  <div class="name">Trang xinh đẹp</div>
+                  <div class="time">Wed Jun 16 2021</div>
+                </div>
+              </div>
+              <div class="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="red"
+                  class="bi bi-suit-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <div class="wrapper-article">
+              <p class="title">title</p>
+              <p class="description">description</p>
+              <p class="readmore">readmore...</p>
+            </div>
+          </div>
+          <!-- end item -->
+
+          <div class="pagination">
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+              <li>3</li>
+            </ul>
+          </div>
+        </div>
+        <div class="tags">
+          <p>Popular Tags</p>
+          <div class="wrapper-tag">
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+            <p class="tag">human</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-github"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+        />
+      </svg>
+      <p>fork on github</p>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.header {
+  display: flex;
+  justify-content: space-around;
+
+  .nav {
+    ul {
+      display: flex;
+      list-style-type: none;
+      li {
+        margin-left: 15px;
+      }
+    }
+  }
+}
+.body {
+  .banner {
+    padding: 50px;
+    background-color: #5cb85c;
+    color: white;
+    h1 {
+      font-size: 50px;
+    }
+  }
+  .wrapper {
+    display: flex;
+    padding: 0 150px;
+    .global-feed {
+      border-bottom: 1px solid green;
+      padding-bottom: 20px;
+      margin-bottom: 0px;
+      display: inline-block;
+    }
+    .pagination {
+      ul {
+        list-style: none;
+        display: flex;
+        li {
+          color: green;
+          padding: 2px 5px;
+          border: 1px solid gray;
+          border-radius: 2px;
+        }
+      }
+    }
+    .articles {
+      text-align: left;
+      flex: 3;
+
+      .item {
+        border-top: 1px solid gray;
+        padding-top: 50px;
+        margin-right: 35px;
+        .wrapper-info1 {
+          flex: 1;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+        .wrapper-article {
+        }
+        .info {
+          display: flex;
+          .avatar {
+            img {
+              width: 40px;
+              border-radius: 50%;
+            }
+          }
+        }
+      }
+    }
+    .line {
+      height: 1px;
+      width: 100%;
+      background-color: gray;
+    }
+  }
+  .tags {
+    background-color: #f3f3f3;
+    flex: 1;
+    .wrapper-tag {
+      display: flex;
+      flex-wrap: wrap;
+    }
+    .tag {
+      display: inline-block;
+      background-color: #818a91;
+      margin: 10px;
+      border-radius: 10px;
+      padding: 2px 10px;
+    }
+  }
+}
+.footer {
+  background-color: grey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+
+
+
+
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "about",
+};
+</script>
